c7b646d69686c8b2215ef8109cc3399e991baaa1
Miguel Borges <mrcb08022002@gmail.com>
2022-12-20 12:22:35 +0000

    Epico

--- src/board.h
+++ src/board.h
@@ -1,5 +1,19 @@
 #pragma once
 
-class Board {
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <map>
 
+class Board {
+public:
+  Board();
+  void draw_board();
+  bool check_win();
+  bool is_playable(int column);
+  void play(int column, char symbol);
+
+private:
+  std::vector<std::vector<char>> slots;
+  std::map<std::string, std::string> colors;
 };
--- src/player.h
+++ src/player.h
@@ -1,2 +1,15 @@
 #pragma once
 
+#include <string>
+#include "board.h"
+
+class Player {
+public:
+  std::string get_name();
+  void play();
+
+private:
+  std::string name;
+  int last_move[2];
+  char symbol;
+};
