08bd5e55fbe3b31f510727f2f0ec8124f40b23bd
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-12-21 16:37:35 +0000

    Revert "Game class developing"

--- src/game.cpp
+++ src/game.cpp
@@ -13,21 +13,15 @@
 
 Game::Game() : input(), board(input.get_input<int>("What's the board height?"), input.get_input<int>("What's the board width?"), input.get_input<int>("How many symbols shouls one connect to win the game?"), 'X', 'O')
 {
-	board_ptr = new Board(0, 0, 0, 'X', 'O');
-	std::array<char, NUMBER_OF_PLAYERS> player_symbols;
-
-	for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
-	{
-		char symbol = input.get_input<char>("Is this player a computer? Y/N ");
-		if (toupper(symbol) == 'Y')
+	for (int i=0; i<NUMBER_OF_PLAYERS; i++)
+	{ 
+		if (toupper(input.get_input<char>("Is this player a computer? Y/N \n")) == 'Y')
 		{
-			player_symbols[i] = symbol;
-			players[i] = new Player(input.get_input<char>("Choose the player's symbol "));
+			players[i] = new Player(input.get_input<char>("Choose the player's symbol \n"));
 		}
-		else if (toupper(symbol) == 'N')
+		else if (toupper(input.get_input<char>("Is this player a computer? Y/N \n")) == 'N')
 		{
-			player_symbols[i] = symbol;
-			players[i] = new Computer(input.get_input<char>("Choose the player's symbol"));
+			players[i] = new Computer(input.get_input<char>("Choose the player's symbol \n"));
 		}
 		else
 		{
@@ -35,29 +29,8 @@ Game::Game() : input(), board(input.get_input<int>("What's the board height?"), 
 			std::cout << "Please provide a valid answer to the question \n";
 		}
 	}
-
-	while (1)
-	{
-		int height = input.get_input<int>("What's the board height?");
-		if (height < BOARD_DIMENSION_UPPER_LIMIT && height>BOARD_DIMENSION_LOWER_LIMIT) break;
-		std::cout << "Please input a number between " << BOARD_DIMENSION_LOWER_LIMIT << " and " << BOARD_DIMENSION_UPPER_LIMIT << std::endl;
-	}
-	while (1)
-	{
-		int width = input.get_input<int>("What's the board width?");
-		if (width < BOARD_DIMENSION_UPPER_LIMIT && width>BOARD_DIMENSION_LOWER_LIMIT) break;
-		std::cout << "Please input a number between " << BOARD_DIMENSION_LOWER_LIMIT << " and " << BOARD_DIMENSION_UPPER_LIMIT << std::endl;
-	}
-	while (1)
-	{
-		int to_connect = input.get_input<int>("How many symbols should one connect to win the game?");
-		if (to_connect > TO_CONNECT_LOWER_LIMIT && to_connect < std::max(height, width)) break;
-		std::cout << "Please input a valid number that is higher than " << TO_CONNECT_LOWER_LIMIT << " and possible, given your board. \n";
-	}
-
-	*board_ptr = Board(width, height, to_connect, player_symbols[0], player_symbols[1]);
-	turn = FIRST_TURN;
-
+	
+	turn = 1;
 	time_t current_time = time(NULL);
 	times = localtime(&current_time);
 }
@@ -68,69 +41,11 @@ Game::~Game()
 
 	out.open("match logs.txt", std::ios_base::app);
 	out << std::setfill('0');
-
-	out << times->tm_year + 1900 << " - " << times->tm_mon + 1 << " - " << times->tm_mday << " / "  //tm_year needs to be converted by adding 1900 and tm_mon by adding 1
-		<< times->tm_hour << ":" << std::setw(2) << times->tm_min << " - 1) " << (players[0])->get_name;
-
-	Computer* tmp_p = dynamic_cast<Computer*> players[0];
-
-	if (tmp_p != NULL)
-	{
-		out << " (CPU - " << tmp_p->get_depth() << ')';
-	}
-	out << " vs 2) " << (players[1])->get_name();
-
-	tmp_p = dynamic_cast<Computer*> players[1];
-	if (tmp_p != NULL)
-	{
-		out << " (CPU - " << tmp_p->get_depth() << ')';
-	}
-	if (board.check_win(players[turn % 2]->get_last_move()))
-	{
-		out << " - vencedor : ";
-		if (turn % 2 == 0)
-		{
-			out << "1) " << (players[0])->get_name();
-		}
-		else
-		{
-			out << "2) " << players[1]->get_name();
-		}
-		
-		tmp_p = dynamic_cast<Computer*> players[turn%2];
-		if (tmp_p != NULL)
-		{
-			out << " (CPU - " << tmp_p->get_depth() << ')';
-		}
-	}
-	else
-	{
-		out << " - Empate";
-	}
-	out << ".\n";
-	out.close();
-
+	out << times->tm_year + 1900 << " - " << times->tm_mon + 1 << " - " << times->tm_mday << " / " 
+		<< times->tm_hour << ":" << std::setw(2) << times->tm_min << " - 1) " << players[0]->get_name();
+	
 	for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
 	{
 		delete players[i];
-	}
-	delete tmp_p;
-}
-
-void Game::loop()
-{
-	while (turn < board_ptr->get_height() * board_ptr->get_width()) {
-		board_ptr->draw_board();
-		(players[(turn - 1) % 2])->play(&(*board_ptr));
-		if (board_ptr->check_win((players[++turn % 2])->get_last_move())) break;
-	}
-	board_ptr->draw_board();
-	if (board_ptr->check_win(players[turn % 2]->get_last_move()))
-	{
-		std::cout << "Winner is " << (players[turn % 2])->get_name() << "! Congratulations!" << std::endl;
-	}
-	else
-	{
-		std::cout << "Draw!" << std::endl;
 	}
 }
--- src/game.h
+++ src/game.h
@@ -1,10 +1,6 @@
 #pragma once
 
 #define NUMBER_OF_PLAYERS 2
-#define BOARD_DIMENSION_UPPER_LIMIT 10
-#define BOARD_DIMENSION_LOWER_LIMIT 6
-#define TO_CONNECT_LOWER_LIMIT 4
-#define FIRST_TURN 1
 
 #include <iostream>
 #include <string>
@@ -21,21 +17,17 @@
 /**
 * Our Game class. This class initializes each match that is played, containing information related
 * to the board, to the players in play and the time of play.
-* It also registers the results after match end.
+* It also registers the results after match end
 */
 class Game {
 public:
-	/** Initialization of the Game class. */
-	Game(); 
-	/** End of match; logs the results, desctructor of Game class. */
-	~Game();
-	/** Loop of game. */
-	void loop(); 
+	Game(); //initialization of the game class
+	~Game(); //end of match; logs the results
 private:
 	InputHandler input;
 	tm* times;
 	std::array<Player* ,2> players;
 	int turn;
-	Board* board_ptr;
+	Board board;
 };
 
