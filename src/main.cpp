8075a7a2dedd2298102e997e47a67cb919012d3c
Miguel Borges <mrcb08022002@gmail.com>
2022-12-22 20:30:50 +0000

    would still play again if it gave an invalid input first.

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ int main() {
 		Game game;
 		game.loop();
 		char play_again = toupper(input.get_input<char>("Do you want to play again? Y/N"));
-		if (play_again == 'N')
-		{
-			break;
-		}
 		while (play_again != 'Y' && play_again != 'N')
 		{
 			std::cout << "Please provide a valid answer to the question \n";
 			play_again = toupper(input.get_input<char>("Do you want to play again? Y/N"));
+		}
+		if (play_again == 'N')
+		{
+			break;
 		}
 
 	} while (1);
