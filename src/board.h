2434f40313673c65b2233e261b68243047af2a12
Miguel Borges <mrcb08022002@gmail.com>
2022-12-22 23:15:21 +0000

    Francisco + Meu labor + pontos finais.

--- src/board.h
+++ src/board.h
@@ -20,33 +20,82 @@
 
 class Board {
 public:
+  /**
+  * Construct of Board.
+  *
+  * @param width Board width.
+  * @param height Board Height.
+  * @param to_connect Defines how many pieces in a line are needed for a win.
+  * @param player_one_symbol Symbol used by Player 1.
+  * @param player_two_symbol Symbol used by Player 2.
+  */
   Board(int width, int height, int to_connect, char player_one_symbol, char player_two_symbol);
+
+  /**
+  * Construct of Board.
+  * Clones an existing board.
+  * Useful for the Ai's board states.
+  * 
+  * @param game_board Pointer to the board that will be cloned.
+  */
   Board(Board &game_board);
+
+  /**
+  * Prints the board on the console, with all the appropriate formatting.
+  */
   void draw_board() const;
+
+  /**
+  * Checks if win conditions are met.
+  *
+  * @param last_move Coordinates of the last placed piece.
+  * @return If a player has won the game.
+  */
   bool check_win(std::array<int, 2> last_move) const;
+
+  /**
+  * Checks if given column has an empty slot.
+  *
+  * @param column Index of given column to check.
+  * @return If column has an empty slot.
+  */
   bool is_playable(int column) const;
-  int get_last_played_column() const;
+
+  /**
+  * Returns list of playable columns.
+  * Important for the AI.
+  * 
+  * @return Vector of the indexes of the columns that haven't been filled.
+  */
   std::vector<int> playable_columns() const;
 
   /**
-  * Method to get width
+  * Returns the last played column.
+  * Important for the AI.
+  * 
+  * @return Index of the last played column.
+  */
+  int get_last_played_column() const;
+
+  /**
+  * Method to get width.
   *
-  * @return Board width 
+  * @return Board width.
   */
   int get_width() const;
 
   /**
-  * Method to get height
+  * Method to get height.
   *
-  * @return Board height
+  * @return Board height.
   */
   int get_height() const;
 
   /**
-  * Returns the player symbol of the given index
+  * Returns the player symbol of the given index.
   *
-  * @param player Index of the player to get the symbol
-  * @return Player symbol
+  * @param player Index of the player to get the symbol.
+  * @return Player symbol.
   */
   char get_symbol(int player) const;
 
@@ -54,16 +103,35 @@ public:
   * Plays the symbol in the given column.
   * Symbol is used to identify the player. (Players dont know their indexes) 
   *  
-  * @param column Column to be played
-  * @param symbol Symbol which will be played
-  * @return row where the symbol was placed
+  * @param column Column to be played.
+  * @param symbol Symbol which will be played.
+  * @return row where the symbol was placed.
   */
   int play(int column, char symbol);
 
 private:
+  /**
+  * Variable which holds the board places.
+  */
   std::vector<std::vector<char>> slots;
+
+  /**
+  * Helper function to draw the board. Draws the board header.
+  */
   void print_header() const;
+
+  /**
+  * Variable that holds the last played column. Needed to be able to caching on the AI.
+  */
   int last_played_column;
+
+  /**
+  * Variable that holds how many symbols has to connect.
+  */
   const int to_connect;
+
+  /**
+  * Array that holds the player symbols. Used for coloring and to know what symbol is going to be placed on the AI.
+  */
   const std::array<char, NUMBER_OF_PLAYERS> symbols;
 };
