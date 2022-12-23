57c644d38333f83c62ae6f3b99183af4849436dc
Francisco Magalhães <66530003+FranciscoCamoesM@users.noreply.github.com>
2022-12-23 14:27:41 +0000

    Added comments

--- src/computer.h
+++ src/computer.h
@@ -5,11 +5,37 @@
 
 class Computer : public Player {
 public:
+  
+  /**
+  * Constructor
+  *
+  *@param symbol_to_set Symbol to represent the computer
+  */
   Computer(char symbol_to_set);
+  
+  /**
+  * Plays according to pre-defined depth
+  *
+  *@param board Current game board
+  */
   void play(Board &board);
+  
+  /**
+  * Method to return depth
+  *
+  *@return depth
+  */
   int get_depth();
 
 private:
+  
+  /**
+  * Number of turns the computer simulates
+  */
   int depth;
+  
+  /**
+  * Pointer corresponding to the game's current State
+  */
   BoardState *state;
 };
