1ce15a524a82f9c87a9300f4c99b5dd23147bf84
Francisco Magalhães <66530003+FranciscoCamoesM@users.noreply.github.com>
2022-12-23 15:13:43 +0000

    Forataçao e pontos finais

--- src/computer.h
+++ src/computer.h
@@ -7,35 +7,35 @@ class Computer : public Player {
 public:
   
   /**
-  * Constructor
+  * Constructor.
   *
-  *@param symbol_to_set Symbol to represent the computer
+  * @param symbol_to_set Symbol to represent the computer.
   */
   Computer(char symbol_to_set);
   
   /**
-  * Plays according to pre-defined depth
+  * Plays according to pre-defined depth.
   *
-  *@param board Current game board
+  * @param board Current game board.
   */
   void play(Board &board);
   
   /**
-  * Method to return depth
+  * Method to return depth.
   *
-  *@return depth
+  * @return depth.
   */
   int get_depth();
 
 private:
   
   /**
-  * Number of turns the computer simulates
+  * Number of turns the computer simulates.
   */
   int depth;
   
   /**
-  * Pointer corresponding to the game's current State
+  * Pointer corresponding to the game's current State.
   */
   BoardState *state;
 };
